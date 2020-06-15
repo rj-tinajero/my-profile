67d599cbd0dd42e49c598795e8f23ec9e1d1cc64
Richard Tinajero <rj.tinajero92@gmail.com>
2020-06-15 12:51:49 -0700

    Updates

---
+++ precache-manifest.7b5ef41c0c33948271c98153bff6f280.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4927e1098bca8343ff0c9d1bfb9496dc",
+    "url": "/my-profile/index.html"
+  },
+  {
+    "revision": "4057edece249f6879613",
+    "url": "/my-profile/static/css/main.a68ba7b2.chunk.css"
+  },
+  {
+    "revision": "08380080bc93a6182b9c",
+    "url": "/my-profile/static/js/2.596197e0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-profile/static/js/2.596197e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4057edece249f6879613",
+    "url": "/my-profile/static/js/main.67e581be.chunk.js"
+  },
+  {
+    "revision": "6d8f0160b3eb678d55ad",
+    "url": "/my-profile/static/js/runtime-main.07a20771.js"
+  },
+  {
+    "revision": "459635ef7daf3537aa4ad513d6ec81e1",
+    "url": "/my-profile/static/media/background-graphic.459635ef.png"
+  },
+  {
+    "revision": "1e7c6416dd42085bd54bdbbaba7b6527",
+    "url": "/my-profile/static/media/icon-github.1e7c6416.svg"
+  },
+  {
+    "revision": "ed4316fe84d6777a59c0b9c11ae7be0b",
+    "url": "/my-profile/static/media/icon-linkedin.ed4316fe.svg"
+  },
+  {
+    "revision": "79475a487e63ebb6a70f34bdad46ce66",
+    "url": "/my-profile/static/media/profile_headshot.79475a48.jpg"
+  }
+]);
